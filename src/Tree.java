b50ce55bfff7d9999514d61435d80327645d9c79
Sushaan Patel <sushaanpatel@gmail.com>
2025-09-16 11:39:29 -0400

    Added delete item method

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,7 @@
 import java.util.Objects;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tree {
     // We recommend attempting this class last, as it hasn't been scaffolded for your team.
@@ -125,26 +126,74 @@ public class Tree {
         } else {
             int[] out = new int[0];
             for (Tree subtree: this.subtrees){
-                out = this.extend(out, subtree.leaves());
+                out = this.extend_int(out, subtree.leaves());
             }
             return out;
         }
     }
 
-    private int[] append(int[] current, int value){
+    private int[] append_int(int[] current, int value){
         int[] newArr = new int[current.length + 1];
         System.arraycopy(current, 0, newArr, 0, current.length);
         newArr[current.length] = value;
         return newArr;
     }
 
-    private int[] extend(int[] arr1, int[] arr2){
+    private Tree[] append_tree(Tree[] current, Tree value){
+        Tree[] newArr = new Tree[current.length + 1];
+        System.arraycopy(current, 0, newArr, 0, current.length);
+        newArr[current.length] = value;
+        return newArr;
+    }
+
+    private int[] extend_int(int[] arr1, int[] arr2){
         int[] newArr = new int[arr1.length + arr2.length];
         for (int val: arr2){
-            arr1 = this.append(arr1, val);
+            arr1 = this.append_int(arr1, val);
         }
         System.arraycopy(arr1, 0, newArr, 0, arr1.length);
         return newArr;
+    }
+
+    private Tree[] extend_tree(Tree[] arr1, Tree[] arr2){
+        Tree[] newArr = new Tree[arr1.length + arr2.length];
+        for (Tree val: arr2){
+            arr1 = this.append_tree(arr1, val);
+        }
+        System.arraycopy(arr1, 0, newArr, 0, arr1.length);
+        return newArr;
+    }
+
+    public boolean deleteItem(int item){
+        if (this.isEmpty()){
+            return false;
+        } else if (this.root == item) {
+            this.deleteRoot();
+            return true;
+        } else {
+            for (int i = 0; i < this.subtrees.length; i++){
+                boolean deleted = this.subtrees[i].deleteItem(item);
+                if (deleted && this.subtrees[i].isEmpty()){
+                    //remove empty subtree
+                    return true;
+                } else if (deleted) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private void deleteRoot(){
+        if (this.subtrees.length == 0){
+            this.root = null;
+        } else {
+            Tree chosen_subtree = this.subtrees[this.subtrees.length - 1];
+            //remove chosen subtree from subtrees
+            this.subtrees = Arrays.copyOf(this.subtrees, this.subtrees.length - 1);
+            this.root = chosen_subtree.root;
+            this.subtrees = this.extend_tree(this.subtrees, chosen_subtree.subtrees);
+        }
     }
 
 }
@@ -153,7 +202,8 @@ class Eh{
     public static void main(String[] args) {
         Tree t1 = new Tree(1, new Tree[]{new Tree(2, new Tree[]{}), new Tree(3, new Tree[]{})});
         Tree t2 = new Tree(1, new Tree[]{new Tree(2, new Tree[]{}), new Tree(3, new Tree[]{new Tree(4, new Tree[]{})})});
-        System.out.println(Arrays.toString(t1.leaves()));
+        System.out.println((t2.deleteItem(1)));
+        System.out.println(t2.repr());
     }
 }
 
